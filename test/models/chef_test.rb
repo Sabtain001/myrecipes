56153df072ee012145a8360c48269d3d861b149d
Sabtain001 <mirzasabtain346@gmail.com>
2018-03-09 15:22:28 +0500

    add chef and recipes model validation

---
+++ test/models/chef_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class ChesfTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.new(chefname: "sabtain", email: "test1@test.com")
+  end
+
+  test "should be valid " do
+    assert @chef.valid?
+  end
+  test "name should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+  test "name should be less than 30 char" do
+    @chef.chefname = "a" * 31
+    assert_not @chef.valid?
+  end
+  test "email should accept correct format" do
+    valid_emails = %w[user@emaple.com sabtain@gmail.com M.first@yahoo.ca john+smith@co.uk.org]
+    valid_emails.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  def setup
+    @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe")
+  end
+
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  test "name shuold be present " do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  test " description name should be present " do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+end
